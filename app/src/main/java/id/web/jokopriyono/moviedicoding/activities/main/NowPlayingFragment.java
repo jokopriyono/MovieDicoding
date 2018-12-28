bce7c947016bd167edfa0e79e861a60b0530c5be
jokopriyono <joe.mandrake007@gmail.com>
2018-12-28 17:27:32 +0700

    - Updating dependencies

--- app/src/main/java/id/web/jokopriyono/moviedicoding/activities/main/NowPlayingFragment.java
+++ app/src/main/java/id/web/jokopriyono/moviedicoding/activities/main/NowPlayingFragment.java
@@ -1,6 +1,7 @@
 package id.web.jokopriyono.moviedicoding.activities.main;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -17,12 +18,12 @@ public class NowPlayingFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_cari, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_now_play, container, false);
     }
 
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
     }
 }
