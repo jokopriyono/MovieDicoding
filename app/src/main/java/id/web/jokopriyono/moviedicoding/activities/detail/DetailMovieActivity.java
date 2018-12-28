7da954cbd240a5f33483589d369e441b25efb1fe
jokopriyono <joe.mandrake007@gmail.com>
2018-12-29 00:23:12 +0700

    - Menambahkan Detail Activity

---
+++ app/src/main/java/id/web/jokopriyono/moviedicoding/activities/detail/DetailMovieActivity.java
@@ -0,0 +1,15 @@
+package id.web.jokopriyono.moviedicoding.activities.detail;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import id.web.jokopriyono.moviedicoding.R;
+
+public class DetailMovieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_movie);
+    }
+}
